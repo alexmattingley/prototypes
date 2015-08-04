b2ae38e226f08ce9012318679c42aa4c13e7d3bc
Alex Mattingley <alexmattingley@gmail.com>
2015-08-04 16:37:47 -0700

    created a firstMovieImg variable

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,8 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result;
+var movieName;
+var firstMovie;
+var firstMovieImg;
 
 $(document).ready(function(){
 	$('button').click(function(){
@@ -9,9 +12,12 @@ $(document).ready(function(){
 			url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
 			success: function(result){
 				console.log('loaded',result);
-
+				global_result = result;
 			}
 		});
+		firstMovie = global_result.feed.entry[0];
+		firstMovieImg = global_result.feed.entry[0]['im:image'][0].label;
+		console.log(firstMovieImg);
 	});
 });
 
