f6dda8030664864b010cb956268f398911a419c2
Alex Mattingley <alexmattingley@gmail.com>
2015-08-17 11:41:14 -0700

    prototype 3 complete, php-includes complete

---
+++ php_includes/includes/main.php
@@ -0,0 +1,15 @@
+<main id="main_content">
+    Main Content
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit qui quo nam, architecto vitae repellat voluptate in illum sapiente eos ipsam reprehenderit modi, incidunt vel repudiandae dignissimos ullam minus deserunt facilis sunt? Nemo vitae, corporis fuga, inventore aliquam sunt unde. Ipsam placeat optio velit temporibus incidunt et vel, sit quidem?</p>
+    <img src="http://lorempixel.com/400/400/" alt="">
+</main>
+<div>I'm not in the main content or the footer</div>
+<footer>
+    <nav id="site_map">
+        <ul>
+            <li>Location</li>
+            <li>Contact Us</li>
+            <li>Hours</li>
+        </ul>
+    </nav>
+</footer>
