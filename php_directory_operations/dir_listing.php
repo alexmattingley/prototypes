96f48d6e20171f6c1638d470c62424331548f33c
Alex Mattingley <alexmattingley@gmail.com>
2015-08-18 15:08:20 -0700

    proto 2 complete

--- php_directory_operations/dir_listing.php
+++ php_directory_operations/dir_listing.php
@@ -1,1 +1,20 @@
-dir_listing.php
+<?php 
+
+	$images_array = glob("images/*.jpg");
+	$success = false;
+	if($images_array === false) {
+		$errors[] = 'glob operation failed';
+	}else if (count($images_array) == 0) {
+		$errors[] = 'There are no files';
+	}
+	else {
+		$errors = [];
+		$success = true;
+	}
+	$output = ['success' => $success, 'errors' => $errors, 'files' => $images_array];
+	echo json_encode($output);
+
+	//print_r($images_array);
+
+// look up how to json encode a php variable
+?>
