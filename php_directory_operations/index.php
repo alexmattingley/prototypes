b901c9210cdf7f023ec8001e737e5ee69b726679
Alex Mattingley <alexmattingley@gmail.com>
2015-08-18 12:47:15 -0700

    all four of these lines do the same thing, this is the four different ways to do this

--- php_directory_operations/index.php
+++ php_directory_operations/index.php
@@ -1,5 +1,23 @@
-<div id="image_container">
-	<?php 
-		print_r(glob("images/*.jpg")); 
-	?>
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8">
+	  <title>index</title>
+	</head>
+	<body>
+		<div id="image_container">
+			<?php 
+				$images_array = glob("images/*.jpg");
+				for($i = 0; $i <= count($images_array); $i++) {
+					//print("<img src='".$images_array[$i]."' alt=''>");
+					//print("<img src='{$images_array[$i]}' alt=''>");
+					?><img src='<?=$images_array[$i];?>' alt=''><?php
+				}
+
+			?>
+
+		</div>
+
+	</body>
+</html>
+
