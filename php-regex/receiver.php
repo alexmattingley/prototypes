180a698a93abf2f3bf49ab8c2299ccae664e616b
Alex Mattingley <alexmattingley@gmail.com>
2015-08-21 08:07:03 -0700

    all regex parts seem to be working, going to extensive testing, and comment and then ill merge

--- php-regex/receiver.php
+++ php-regex/receiver.php
@@ -8,22 +8,38 @@ foreach ($_POST as $key => $value) {
 		case 'first_name':
 			if(preg_match('/.{2,}/', $value)){
 				print("<br>checking $key: " . $value . '<br>');
-			}
-			else{
+			}else{
 				print('<br>Thats not a valid name entry<br>');
 			}
 			break;
 		case 'age':
-			print("checking $key: " . $value . '<br>');
+			if(preg_match('/[0-9]{1,3}/', $value)){
+				print("checking $key: " . $value . '<br>');
+			}else{
+				print("Wow your old, like really really old. Maybe try to enter your actual age? cause I know your not $value");
+			}
 			break;
 		case 'phone':
-			print("checking $key: " . $value . '<br>');
+			if (preg_match('/([0-9]{3} [0-9]{3} [0-9]{4})|([0-9]{3}-[0-9]{3}-[0-9]{4})|([1]{1}[0-9]{3} [0-9]{3} [0-9]{4})|(\([0-9]{3}\) [0-9]{3}-[0-9]{4})/', $value)) {
+				print("checked $key: " . $value . ' ...Your good!<br>');
+			}else{
+				print('we are only testing for American numbers. You either wrote an unmurican number, or you wrote your number weird<br>');
+			}
 			break;
 		case 'username':
-			print("checking $key: " . $value . '<br>');
+			if(preg_match('/(^[a-zA-Z])([a-zA-Z0-9]{5})/', $value)){
+				print("checking $key: " . $value . '<br>');
+			}else{
+				print('your username should be 6 characters long, start with a letter and contain nothing but letters and numbers');
+			}
 			break;
 		case 'bonus':
-			print("checking $key: " . $value . '<br>');
+			if(preg_match('/[0-9]{3}/', $value)){
+				print("checking $key: " . $value . '<br>');
+			}else {
+				print("why you no have three consecutive numbers");
+			}
+			
 			break;
 		default:
 			print('everything else<br>');
