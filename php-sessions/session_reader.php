e6632d2e9dac04b14cab751eba3b53b62fdd6cfd
Alex Mattingley <alexmattingley@gmail.com>
2015-08-21 16:23:36 -0700

    dynamic counting and outputting of the error_index

--- php-sessions/session_reader.php
+++ php-sessions/session_reader.php
@@ -2,9 +2,10 @@
     session_start();
     $_SESSION = $_POST;
     print_r($_SESSION);
+    $error_index = 0;
 
     foreach ($_SESSION as $key => $value) {
-        $error_index = 0;
+        
         switch($key){
             case 'name':
                 if(preg_match('/.{2,}/', $value)){
@@ -12,21 +13,22 @@
                 }else{
                     $_SESSION['errors'][$error_index] = 'you name entered is invalid';
                     $error_index++;
-                    print($error_index);
                 }
             break;
             case 'age':
                 if(preg_match('/(^[0-9]{1,3}$)/', $value)){
                     print("<br>checking $key: " . $value . '<br>');
                 }else{
-                    print("<br>Wow your old, like really really old. Maybe try to enter your actual age? cause I know your not $value<br>");
+                    $_SESSION['errors'][$error_index] = "Wow your old, like really really old. Maybe try to enter your actual age?";
+                    $error_index++;
                 }
             break;
             case 'occupation':
                 if(preg_match('/.{2,}/', $value)){
                     print("<br>checking occupation: $value<br>");
                 }else{
-                    print("<br>occupation invalid");
+                    $_SESSION['errors'][$error_index] = "occupation invalid";
+                    $error_index++;
                 }
             break;
         }
