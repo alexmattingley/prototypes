f9ebd7ac0dd971900e274593803b64f047701f95
Alex Mattingley <alexmattingley@gmail.com>
2015-08-21 11:33:34 -0700

    Have the session_reader file working

---
+++ php-sessions/session_reader.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    $_SESSION = $_POST;
+    print_r($_SESSION);
+?>
---
+++ php-sessions/session_setter.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Session Setter</title>
+</head>
+<body>
+
+    <form action="session_reader.php" method="POST">
+        <input type="text" name="Name">
+        <input type="text" name="Age">
+        <input type="text" name="occupation">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
