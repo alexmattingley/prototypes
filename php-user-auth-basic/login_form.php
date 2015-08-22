d3e5fdacdc134b49c8cd2a897bae66aef57d7b03
Alex Mattingley <alexmattingley@gmail.com>
2015-08-22 13:12:41 -0700

    basic html structure and a click handler

--- php-user-auth-basic/login_form.php
+++ php-user-auth-basic/login_form.php
@@ -1,2 +1,21 @@
 <!--login_form.php-->
-<input type="text">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>login_form</title>
+</head>
+<body>
+    <form action="login_handler.php">
+        <input type="text" name="username">
+        <input type="text" name="password">
+        <button id="login_button" type="button">Login</button>
+    </form>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script>
+        $('#login_button').click(function(){
+            console.log('you clicked me');
+        });
+    </script>
+</body>
+</html>
