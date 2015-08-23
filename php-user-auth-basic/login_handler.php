210102951ad770dbe3c9afe8fc1774568fb0faeb
Alex Mattingley <alexmattingley@gmail.com>
2015-08-22 17:32:55 -0700

    $user_info array, and a foreach loop checking username against the user_info array

--- php-user-auth-basic/login_handler.php
+++ php-user-auth-basic/login_handler.php
@@ -1,4 +1,22 @@
 <!--login_handler.php-->
 <?php
     print_r($_POST);
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $user_info = [
+        ['id'=> 0, 'username'=>'dpaschal', 'password' => 'fluffybunnies'],
+        ['id'=> 1, 'username'=>'rickybobby', 'password' => 'shakeandbake'],
+        ['id'=> 2, 'username'=>'darkhelmet', 'password' => '12345'],
+        ['id'=> 3, 'username'=>'isprout', 'password' => 'ilovewenches'],
+        ['id'=> 4, 'username'=>'goggles', 'password'=>'eatshitasshole']
+    ];
+
+    foreach ($user_info as  $key => $value) {
+        if($username == $value['username']){
+            print("we have a match, welcome $username");
+        }
+    }
+
 ?>
