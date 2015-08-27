9c091d3a3d1a156cbb9c33965caaa9c133cc070b
Alex Mattingley <alexmattingley@gmail.com>
2015-08-27 10:49:11 -0700

    proto 1

--- mysql-basics/mysql_queries.sql
+++ mysql-basics/mysql_queries.sql
@@ -1,3 +1,9 @@
 Place your queries below
 
 SELECT * FROM Users WHERE username = "amattingley"
+
+UPDATE Users SET email="iamwhamo@gmail.com" WHERE username="rmattingley"
+
+INSERT INTO users SET username="santaclaus",email="northpolebro@ic.com",password="password"
+
+DELETE FROM Users WHERE username="santaclaus"
