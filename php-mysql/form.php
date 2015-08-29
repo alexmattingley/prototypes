e4395618f659770edeb47b9c6be7cf170863d45e
Alex Mattingley <alexmattingley@gmail.com>
2015-08-29 13:36:40 -0700

    dynamically implementing data from the form, going to experiment with the datepicker from jquery

--- php-mysql/form.php
+++ php-mysql/form.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <style>
     input, textarea{
--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -3,9 +3,19 @@
 <!--So this doc only works on refresh-->
 <?php
 print_r($_POST);
+$title = $_POST['title'];
+$details = $_POST['details'];
+$timestamp = $_POST['timestamp'];
+$favColor = $_POST['favorite_color'];
+print("$title $details $timestamp $favColor<br>");
 require('mysql_connect.php');
-$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (8,'run','workout',CURRENT_TIME,'luliferr')";
+$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (10, '$title','$details', CURRENT_DATE ,'luliferr')";
 $results = mysqli_query($conn, $query);
-print(mysqli_affected_rows($conn) > 0);
-print_r($results);
+if(mysqli_affected_rows($conn) > 0){
+    print('mysqli_affected_rows is true');
+    print_r($results);
+}else {
+    print('something is not working');
+}
+
 ?>
