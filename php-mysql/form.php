374374d0eeafb8c6b40b1f6a95a587b4792e3d9b
Alex Mattingley <alexmattingley@gmail.com>
2015-08-29 13:45:13 -0700

    datepicker working, gonna see if it will integrate with the index_insert

--- php-mysql/form.php
+++ php-mysql/form.php
@@ -3,8 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <style>
     input, textarea{
@@ -16,9 +17,14 @@
     <form action="index_insert.php" method="POST">
         <input name="title" type="text" placeholder="Title">
         <textarea name="details" id="details" cols="30" rows="10" placeholder="Details"></textarea>
-        <input type="text" name="timestamp" placeholder="timestamp">
+        <input type="text" name="timestamp" id="timestamp" placeholder="timestamp">
         <input type="text" name="favorite_color" placeholder="favorite color?">
         <button>Submit</button>
     </form>
+    <script>
+        $(function() {
+            $( "#timestamp" ).datepicker();
+        });
+    </script>
 </body>
 </html>
