bb1a79a84d5cbc061e358d35b11df28b5fb16552
Alex Mattingley <alexmattingley@gmail.com>
2015-08-28 17:50:31 -0700

    proto-3 complete

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,8 +1,10 @@
 <!-- index_insert.php -->
+
+<!--So this doc only works on refresh-->
 <?php
 require('mysql_connect.php');
-$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (7,'run','workout',CURRENT_TIME,'luliferr')";
+$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (8,'run','workout',CURRENT_TIME,'luliferr')";
 $results = mysqli_query($conn, $query);
 print(mysqli_affected_rows($conn) > 0);
-
+print_r($results);
 ?>
