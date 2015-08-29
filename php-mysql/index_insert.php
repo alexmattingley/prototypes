f46dd19d2394b65d750f5bd63d602e015b45812b
Alex Mattingley <alexmattingley@gmail.com>
2015-08-29 15:34:31 -0700

    proto 4 complete

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -9,7 +9,7 @@ $timestamp = $_POST['timestamp'];
 $favColor = $_POST['favorite_color'];
 print("$title $details $timestamp $favColor<br>");
 require('mysql_connect.php');
-$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (10, '$title','$details', $timestamp ,'luliferr')";
+$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (11, '$title','$details', $timestamp ,'luliferr')"; //how do I auto increment the id here?
 $results = mysqli_query($conn, $query);
 if(mysqli_affected_rows($conn) > 0){
     print('mysqli_affected_rows is true');
