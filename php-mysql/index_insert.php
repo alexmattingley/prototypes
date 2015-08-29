11614a7979e6213356d041425a53a0c55aa7e273
Alex Mattingley <alexmattingley@gmail.com>
2015-08-28 17:21:35 -0700

    proto-1 complete

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,4 +1,8 @@
 <!-- index_insert.php -->
 <?php
 require('mysql_connect.php');
+$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (7,'run','workout',CURRENT_TIME,'luliferr')";
+$results = mysqli_query($conn, $query);
+print(mysqli_affected_rows($conn) > 0);
+
 ?>
--- php-mysql/index_select.php
+++ php-mysql/index_select.php
@@ -1,1 +1,13 @@
 <!-- index_select.php -->
+<?php
+require('mysql_connect.php');
+$query = "SELECT * FROM second_todo_items";
+$results = mysqli_query($conn, $query);
+if(mysqli_num_rows($results) > 0){
+    while($result = mysqli_fetch_assoc($results)){
+        $output[] = $result;
+    }
+    print_r($output);
+}
+
+?>
