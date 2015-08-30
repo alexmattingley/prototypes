08830366da014632cef4ade9622b1ab3bda53926
Alex Mattingley <alexmattingley@gmail.com>
2015-08-29 18:02:52 -0700

    logout.php working, created an ajax call so when you click the logout button it destroys the session variable

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -2,22 +2,46 @@
 session_start();
 print_r($_SESSION);
 ?>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <?php
-//need to check session at user id before I actually run the below.
-print_r($_POST);
-$title = $_POST['title'];
-$details = $_POST['details'];
-$timestamp = $_POST['timestamp'];
-$favColor = $_POST['favorite_color'];
-$user_id = $_SESSION['user_id'];
-require('mysql_connect.php');
-$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (16, '$title','$details', $timestamp ,'$user_id')"; //how do I auto increment the id here?
-$results = mysqli_query($conn, $query);
-if(mysqli_affected_rows($conn) > 0){
-    print('mysqli_affected_rows is true');
-    print_r($results);
-}else {
-    print('something is not working');
+if(!empty($_SESSION['user_id'])){
+    print_r($_POST);
+    $title = $_POST['title'];
+    $details = $_POST['details'];
+    $timestamp = $_POST['timestamp'];
+    $favColor = $_POST['favorite_color'];
+    $user_id = $_SESSION['user_id'];
+    require('mysql_connect.php');
+    $query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (16, '$title','$details', $timestamp ,'$user_id')"; //how do I auto increment the id here?
+    $results = mysqli_query($conn, $query);
+    if(mysqli_affected_rows($conn) > 0){
+        print('mysqli_affected_rows is true');
+        print_r($results);
+    }else {
+        print('something is not working');
+    }
+}else{
+    print('you are either logged out or your credentials are wrong');
 }
-
 ?>
+<button>Logout</button>
+<script>
+    function logout_call(){
+        $.ajax({
+           url: 'logout.php',
+           method: "POST",
+           cache: false,
+            dataType:'text',
+            success: function(response){
+                console.log("response:" , response);
+            }
+        })
+    }
+    $('button').click(function(){
+        logout_call();
+    });
+
+</script>
+
+
+
