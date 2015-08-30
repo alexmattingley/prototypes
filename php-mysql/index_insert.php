be5c148cec1f38b6174dcb98e27a1a633ba95bf2
Alex Mattingley <alexmattingley@gmail.com>
2015-08-29 17:16:55 -0700

    fixed the error, now I need to create the logout and destroy the session

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,8 +1,7 @@
 <?php
-include('login_handler.php');
+session_start();
+print_r($_SESSION);
 ?>
-
-<!--So this doc only works on refresh-->
 <?php
 //need to check session at user id before I actually run the below.
 print_r($_POST);
@@ -11,9 +10,8 @@ $details = $_POST['details'];
 $timestamp = $_POST['timestamp'];
 $favColor = $_POST['favorite_color'];
 $user_id = $_SESSION['user_id'];
-print("$title $details $timestamp $favColor<br>");
 require('mysql_connect.php');
-$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (15, '$title','$details', $timestamp ,'$user_id')"; //how do I auto increment the id here?
+$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (16, '$title','$details', $timestamp ,'$user_id')"; //how do I auto increment the id here?
 $results = mysqli_query($conn, $query);
 if(mysqli_affected_rows($conn) > 0){
     print('mysqli_affected_rows is true');
