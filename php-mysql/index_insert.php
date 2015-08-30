0d32f0ddb21f89e5dbce9a9f4f13d66755411565
Alex Mattingley <alexmattingley@gmail.com>
2015-08-29 17:14:44 -0700

    this actually works, it just throws a weird error

--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -4,20 +4,22 @@ include('login_handler.php');
 
 <!--So this doc only works on refresh-->
 <?php
-//print_r($_POST);
-//$title = $_POST['title'];
-//$details = $_POST['details'];
-//$timestamp = $_POST['timestamp'];
-//$favColor = $_POST['favorite_color'];
-//print("$title $details $timestamp $favColor<br>");
-//require('mysql_connect.php');
-//$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (11, '$title','$details', $timestamp ,'luliferr')"; //how do I auto increment the id here?
-//$results = mysqli_query($conn, $query);
-//if(mysqli_affected_rows($conn) > 0){
-//    print('mysqli_affected_rows is true');
-//    print_r($results);
-//}else {
-//    print('something is not working');
-//}
-//
+//need to check session at user id before I actually run the below.
+print_r($_POST);
+$title = $_POST['title'];
+$details = $_POST['details'];
+$timestamp = $_POST['timestamp'];
+$favColor = $_POST['favorite_color'];
+$user_id = $_SESSION['user_id'];
+print("$title $details $timestamp $favColor<br>");
+require('mysql_connect.php');
+$query = "INSERT INTO `second_todo_items`(`id`, `title`, `details`, `timestamp`, `user_id`) VALUES (15, '$title','$details', $timestamp ,'$user_id')"; //how do I auto increment the id here?
+$results = mysqli_query($conn, $query);
+if(mysqli_affected_rows($conn) > 0){
+    print('mysqli_affected_rows is true');
+    print_r($results);
+}else {
+    print('something is not working');
+}
+
 ?>
